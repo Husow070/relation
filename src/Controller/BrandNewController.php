efbfa28aa301add5e23320332691681d9f668332
Husow070 <71512853+Husow070@users.noreply.github.com>
2022-11-23 10:30:46 +0100

    1

--- src/Controller/BrandNewController.php
+++ src/Controller/BrandNewController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class BrandNewController extends AbstractController
 {
     #[Route('/brand/new', name: 'app_brand_new')]
-    public function index(): Response
+    public function index(CategoryRepository): Response
     {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+        $categories = $p>findALL()
+
         return $this->render('brand_new/index.html.twig', [
             'controller_name' => 'BrandNewController',
         ]);
